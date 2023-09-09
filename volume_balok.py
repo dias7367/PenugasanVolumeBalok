7f4cfd92d11c0c5b42271eb3efea105811adc0a9
dias7367 <143860024+dias7367@users.noreply.github.com>
2023-09-10 00:22:02 +0700

    Add files via upload

---
+++ volume_balok.py
@@ -0,0 +1,11 @@
+#menghitung volume balok
+
+print("program menghitung volume balok")
+
+p = int(input("masukkan panjang balok: "))
+l = int(input("masukkan lebar balok: "))
+t = int(input("masukkan tinggi balok: "))
+
+V = p*l*t
+print("rumus menghitung volume balok V = pxlxt")
+print("hasil dari hitung volume balok: ", V)
